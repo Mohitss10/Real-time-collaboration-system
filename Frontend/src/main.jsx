5d9781aad0c9f3e0e548ac894ed1ae43d944c9bd
mohitsaysshello <gawandemohit54@gmail.com>
2026-06-04 18:52:28 +0530

    add start script for render

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./app.css";
+import "./App"
 
 const savedTheme =
   localStorage.getItem("theme") || "light";
