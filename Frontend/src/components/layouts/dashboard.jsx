38f00e78dda8563dd9f864cf0a8c346552449bb3
mohitsaysshello <gawandemohit54@gmail.com>
2026-06-04 19:00:12 +0530

    add start script for render

--- Frontend/src/components/layouts/dashboard.jsx
+++ Frontend/src/components/layouts/dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "./sidebar";
 import Navbar from "../../Pages/Navbar";
 import AIChatbot from "../ai/AIChatbot";
 
